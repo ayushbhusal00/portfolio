7e898e94cdabdf7d0b7fa0a50f8aedcca8dea63f
Ayush Bhusal <ayushbhusal00@gmail.com>
2024-10-11 12:20:26 +0545

    Enable Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeContextProvider from "@/context/theme-context";
 import ThemeSwitch from "@/components/theme-switch";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -45,6 +46,7 @@ export default function RootLayout({
         </ThemeContextProvider>
         <div className='bg-[#fbe2e3] dark:bg-[#946263] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] dark:bg-[#676394] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Analytics />
       </body>
     </html>
   );
