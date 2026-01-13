bfaa3e0ecaba8afc2134ccfcb0d266000099b2b5
Ayush Bhusal <ayushbhusal00@gmail.com>
2026-01-13 09:59:17 +0545

    changed clarity from component to header usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 import ClientProviders from "@/components/ClientProviders";
-import MicrosoftClarity from "@/components/MicrosoftClarity";
+// import MicrosoftClarity from "@/components/MicrosoftClarity";
 
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const manropeGX = localFont({
   src: "./fonts/ManropeGX.woff2",
@@ -27,6 +28,17 @@ export default function RootLayout({
     <html lang='en' className='scroll-smooth'>
       <head>
         <link rel='icon' type='image/x-icon' href='/emoji.svg' />
+        <Script
+          type='text/javascript'
+          id='microsoft-clarity'
+          strategy='afterInteractive'
+        >
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v0mbtxb1ye");`}
+        </Script>
       </head>
 
       <body
@@ -34,7 +46,7 @@ export default function RootLayout({
         style={{ fontFamily: "var(--font-manrope), sans-serif" }}
       >
         {/* Microsoft Clarity (Client Only) */}
-        <MicrosoftClarity />
+        {/* <MicrosoftClarity /> */}
 
         {/* App Providers */}
         <ClientProviders>{children}</ClientProviders>
